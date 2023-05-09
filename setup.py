108910abe896c01743fc4960eb267d543a79931d
Facundo Pareja <facundopareja@gmail.com>
2023-05-09 13:39:30 -0300

    fix setup file

--- setup.py
+++ setup.py
@@ -7,4 +7,7 @@ setup(
     version='1.0',
     description='Service for interacting with user goals',
     author='Grupo 5',
+    packages=[''],
+    include_package_data=True,
+    exclude_package_data={'': ['tests', 'kubernetes']},
 )
